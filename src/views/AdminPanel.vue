f2787eaabb32e22c997fceacb2400424d9b0442d
fishu <pkaczoruk@wp.pl>
2019-09-03 13:56:50 +0200

    adjusted nav and simplified code

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -9,29 +9,26 @@
 
     <!-- navigation through sections -->
     <nav id="section-nav">
-      <v-toolbar class="green" dense flat>
-        <div>
-          <v-btn tile text class="nav-btn mx-2">Ilustracje</v-btn>
-        </div>
-        <div>
-          <v-btn tile text class="nav-btn mx-2">Design</v-btn>
-        </div>
-        <div>
-          <v-btn tile text class="nav-btn mx-2">Inne</v-btn>
-        </div>
-        <div>
-          <v-btn tile text class="nav-btn mx-2">O mnie</v-btn>
-        </div>
-        <div>
-          <v-btn tile text class="nav-btn mx-2">Logo</v-btn>
-        </div>          
+      <v-toolbar class="green" height="35px" flat>
+        <v-toolbar-items>
+          <div v-for="(el, i) in navigation" :key="i">
+            <v-btn 
+              @click="show(el.path)"
+              height="100%" 
+              tile 
+              text 
+              class="nav-btn mx-2"
+              :class="[toggle[el.path] ? 'active' : '']"
+            >{{ el.name }}</v-btn>
+          </div>
+        </v-toolbar-items> 
       </v-toolbar>
     </nav>
 
     <!-- ilustracje -->
-    <v-content>
+    <v-content v-if="toggle.ilustracje" id="ilustracje-section">
       <v-container>
-        <div class="d-flex justify-space-around flex-wrap" id="ilust-section">
+        <div class="d-flex justify-space-around flex-wrap">
           <v-card class="display-card my-4" min-height="293px" min-width="220px" max-width="25vw">
             <v-img class="display-img" src="..\assets\luca.png">
               <v-card-title class="justify-space-between">
@@ -41,17 +38,37 @@
             </v-img>
           </v-card>
           <v-card class="display-card my-4" min-height="293px" min-width="220px" max-width="25vw">
-            <v-img class="display-img" src="..\assets\sparrow.jpg"></v-img>
+            <v-img class="display-img" src="..\assets\luca.png">
+              <v-card-title class="justify-space-between">
+                <v-btn x-small fab><v-icon>mdi-pen</v-icon></v-btn>
+                <v-btn x-small fab><v-icon>mdi-delete</v-icon></v-btn>
+              </v-card-title>
+            </v-img>
           </v-card>
           <v-card class="display-card my-4" min-height="293px" min-width="220px" max-width="25vw">
-            <v-img class="display-img" src="..\assets\sparrow.jpg"></v-img>
+            <v-img class="display-img" src="..\assets\sparrow.jpg">
+              <v-card-title class="justify-space-between">
+                <v-btn x-small fab><v-icon>mdi-pen</v-icon></v-btn>
+                <v-btn x-small fab><v-icon>mdi-delete</v-icon></v-btn>
+              </v-card-title>
+            </v-img>
           </v-card>
           <v-card class="display-card my-4" min-height="293px" min-width="220px" max-width="25vw">
-            <v-img class="display-img" src="..\assets\sparrow.jpg"></v-img>
-          </v-card>     
+            <v-img class="display-img" src="..\assets\fox.png">
+              <v-card-title class="justify-space-between">
+                <v-btn x-small fab><v-icon>mdi-pen</v-icon></v-btn>
+                <v-btn x-small fab><v-icon>mdi-delete</v-icon></v-btn>
+              </v-card-title>
+            </v-img>
+          </v-card>
           <v-card class="display-card my-4" min-height="293px" min-width="220px" max-width="25vw">
-            <v-img class="display-img" src="..\assets\fox.png"></v-img>
-          </v-card>                
+            <v-img class="display-img" src="..\assets\sparrow.jpg">
+              <v-card-title class="justify-space-between">
+                <v-btn x-small fab><v-icon>mdi-pen</v-icon></v-btn>
+                <v-btn x-small fab><v-icon>mdi-delete</v-icon></v-btn>
+              </v-card-title>
+            </v-img>
+          </v-card>
         </div>       
       </v-container>
      
@@ -82,9 +99,33 @@
 export default {
   data() {
     return {
-      
+      toggle: {
+        ilustracje: true,
+        design: false,
+        inne: false,
+        about: false,
+        pozostale: false
+      },
+      navigation: [
+        {name: 'ilustracje', path: 'ilustracje'},
+        {name: 'design', path: 'design'},
+        {name: 'inne', path: 'inne'},
+        {name: 'o mnie', path: 'about'},
+        {name: 'pozostałe', path: 'pozostale'}
+      ]
     }
-  }  
+  },
+  methods: {
+    show: function(section) {
+      for(var key in this.toggle) {
+        if(key === section) {
+          this.toggle[key] = true;
+        } else {
+          this.toggle[key] = false;
+        }
+      }
+    }
+  }
 }
 </script>
 
@@ -113,6 +154,8 @@ export default {
     text-align: center;
     display: none;
   }
-
+  .active {
+    background: rgb(3, 144, 3);
+  }
 
 </style>
